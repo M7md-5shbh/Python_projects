cc290477e1a8bfbeb16f8c196e60237a49a68408
Nour <112010512+M7md-5shbh@users.noreply.github.com>
2025-01-01 06:04:38 +0300

    Create Hurdle 1.py

---
+++ Reeborg's World/Hurdle 1.py
@@ -0,0 +1,19 @@
+def turn_right():
+    for i in range(3):
+        turn_left()
+
+def jump():    
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    
+
+move()
+jump()
+for i in range(5):
+    turn_left()
+    move()
+    jump()
