7feeaf8d80c46cdc2623b3cd7d27dbfe5205a21b
Nour <112010512+M7md-5shbh@users.noreply.github.com>
2025-01-01 18:06:07 +0300

    Update Hurdle 4.py
    Simplified the conditions and adjusted the jump() function to work correctly with this level

--- Reeborg's World/Hurdle 4.py
+++ Reeborg's World/Hurdle 4.py
@@ -8,16 +8,21 @@ def turn_right():
     for i in range(3):
         turn_left()
 
-while not at_goal():
-    if front_is_clear() and right_is_clear():
-        turn_right()
-        move()
-    elif front_is_clear():
-        move()
-    elif (not front_is_clear() and not right_is_clear()
-    and a_path_is_clear()):
+def jump():
+    while not front_is_clear():
         turn_left()
-    elif (not front_is_clear() and right_is_clear()):
-        turn_right()
+    while not right_is_clear() and front_is_clear():
         move()
-          
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+
+while not at_goal():
+    if front_is_clear() and not right_is_clear():
+        move()
+    elif a_path_is_clear():
+        jump()
+    
