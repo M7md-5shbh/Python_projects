dc02b971dbc27bec0eb3518c239d7ee734631418
Nour <112010512+M7md-5shbh@users.noreply.github.com>
2025-01-01 19:50:58 +0300

    Create Newspaper 1.py

---
+++ Reeborg's World/Newspaper 1.py
@@ -0,0 +1,34 @@
+def a_path_is_clear():
+    if wall_in_front() and wall_on_right():
+        return True
+    else:
+        return False
+    
+def turn_right():
+    for i in range(3):
+        turn_left()
+def go_right():
+    turn_right()
+    move()
+    move()
+
+go_right_count = 0
+take()
+turn_left()
+move()
+while not at_goal():
+    if wall_in_front() and right_is_clear():
+        go_right()
+        go_right_count +=1
+        if go_right_count > 2 and carries_object():
+            while object_here():
+                take()
+            else: 
+                try:
+                    put("star")
+                except:
+                    continue
+    elif a_path_is_clear():
+        turn_left()
+    elif front_is_clear():
+        move()
