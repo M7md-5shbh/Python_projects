e0db8d7ffedffe18014ad3de06cef4fb0c422ec1
Nour <112010512+M7md-5shbh@users.noreply.github.com>
2025-01-02 22:24:42 +0300

    Create simple calc.py

---
+++ Projects/simple calc.py
@@ -0,0 +1,58 @@
+print(r"""
+ _____________________
+|  _________________  |
+| |              0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ '.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ '.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   '._____.'  | || ||____|  |____|| || |  |________|  | || |   '._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+""")
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+
+def calculator():
+    value = float(input("What's the first number? \n"))
+
+    while True:
+        print(f"What's the operation? ")
+        for key in operations:
+            print(key, end="\n")
+        operation = input()
+        second = float(input("What's the second number? \n"))
+        result = operations[operation](value, second)
+        print(f"{value} {operation} {second} = {result}")
+
+        keep_going = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
+        if keep_going == "n":
+            print("Goodbye!")
+            break
+        elif keep_going == "y":
+            value = result
+
+
+calculator()
