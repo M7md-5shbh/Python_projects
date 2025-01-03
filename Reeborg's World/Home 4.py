1875f176af26b29dfd6b63ef9174ce934aad45fd
Nour <112010512+M7md-5shbh@users.noreply.github.com>
2025-01-04 02:35:58 +0300

    Update Home 4.py
    added another code to do this world

--- Reeborg's World/Home 4.py
+++ Reeborg's World/Home 4.py
@@ -12,3 +12,26 @@ for i in range(50):
     elif wall_in_front() and right_is_clear():
         turn_right()
     
+# in the code above, I used a range higher than the actual size and informed it to stop when it reaches a goal at a corner with walls in front and right
+# to make it simpler, I wrote another code to do the same job, with a function called L_turn as the path is made of L-shaped paths running the same function 4 times to reach the goal
+
+def turn_right():
+    for i in range(3):
+        turn_left()
+
+def L_turn():
+    while True:
+        if front_is_clear():
+            move()
+        elif not front_is_clear():
+            if not right_is_clear():
+                if at_goal():
+                    done()
+                turn_left()
+            else:
+                turn_right()
+                move()
+                turn_right()
+                break
+for i in range(4):
+    L_turn()
